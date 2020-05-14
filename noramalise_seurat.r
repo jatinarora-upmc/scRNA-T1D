29bcf9adf5d3f3655ffa07f347774e4f7aaa7bdf
Jatin Arora <jatinarora.ymca@gmail.com>
2020-05-13 20:34:25 -0400

    scTransform the data and get variable genes

--- noramalise_seurat.r
+++ noramalise_seurat.r
@@ -1,14 +1,15 @@
+# function to normalise (SCtransform) data in seurat object and find given number of variable genes
 # normalise (sctransform) and get variable genes
 # pbmc.fun is seurat object
-# nGenes is the number of variable genes u want
+# nGenes is the number of variable genes you want
 
 normalise_seurat <- function(pbmc.fun, nGenes, var.toRegress){
   # normalise, and regress-out the effect of variable (if given)
   print("normalising...")
   if(is.null(var.toRegress)){
-    pbmc.fun = SCTransform(object = pbmc.fun, verbose = T, do.scale = F, do.center = F, new.assay.name = "SCT")
+    pbmc.fun = SCTransform(object = pbmc.fun, assay = "RNA", verbose = T, do.scale = F, do.center = F, new.assay.name = "SCT")
   } else {
-    pbmc.fun = SCTransform(object = pbmc.fun, verbose = T, do.scale = F, do.center = F, new.assay.name = "SCT", vars.to.regress = var.toRegress)
+    pbmc.fun = SCTransform(object = pbmc.fun, assay = "RNA", verbose = T, do.scale = F, do.center = F, new.assay.name = "SCT", vars.to.regress = var.toRegress)
   }
   
   # print summary
